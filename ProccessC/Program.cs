6a852ce5c15843b6dac51bbca21fb6c161dddb1a
Franco Pettigrosso <fpettigrosso@holyfamily.edu>
2019-05-14 10:37:27 -0400

    code for the programs
    code for the programs

--- ProccessC/Program.cs
+++ ProccessC/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
 
-namespace ProccessC
+class Program
 {
-    class Program
+    // Process C:
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        try
         {
-            Console.WriteLine("Hello World!");
+            using (MemoryMappedFile mmf = MemoryMappedFile.OpenExisting("testmap"))
+            {
+
+                Mutex mutex = Mutex.OpenExisting("testmapmutex");
+                mutex.WaitOne();
+
+                using (MemoryMappedViewStream stream = mmf.CreateViewStream(2, 0))
+                {
+                    BinaryWriter writer = new BinaryWriter(stream);
+                    writer.Write(1);
+                }
+                mutex.ReleaseMutex();
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Memory-mapped file does not exist. Run Process A first, then B.");
         }
     }
 }
