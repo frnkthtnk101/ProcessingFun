f05442ded17e3e9e8b92b467c81f87a9be571974
Franco Pettigrosso <fpettigrosso@holyfamily.edu>
2019-05-14 10:17:01 -0400

    create dotnet project
    I created the dotnet project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessingFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
