d9310566328d1cfa94fd52ee3620b629c025bf76
Franco Pettigrosso <fpettigrosso@holyfamily.edu>
2019-05-14 10:32:28 -0400

    Project files
    Added project files

---
+++ ProccessA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProccessA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ProccessC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProccessC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
